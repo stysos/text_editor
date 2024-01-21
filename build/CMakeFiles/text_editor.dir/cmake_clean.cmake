89c1a092870c4e898f461f97402bd6b7b38a91b4
stysos <samtyson555@gmail.com>
2024-01-21 20:07:02 +0000

    initial commit

---
+++ build/CMakeFiles/text_editor.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/text_editor.dir/main.cpp.o"
+  "CMakeFiles/text_editor.dir/main.cpp.o.d"
+  "text_editor"
+  "text_editor.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/text_editor.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/styson/cpp/text-editor
+# Build directory: /home/styson/cpp/text-editor/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
