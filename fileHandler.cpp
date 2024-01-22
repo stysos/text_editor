3b5eba3ecc1c15c3b15ca1b09fd0795224fb0999
stysos <samtyson555@gmail.com>
2024-01-22 19:29:31 +0000

    initial test of cli (probably a better method to come)

--- fileHandler.cpp
+++ fileHandler.cpp
@@ -1,24 +1,98 @@
 #include "fileHandler.hpp"
 
+#include <termios.h>
+#include <unistd.h>
+
 #include <iostream>
+#include <sstream>
 
-FileHandler::FileHandler(std::string fileName) {
-  this->fileInfo.fileName = fileName;
+FileHandler::FileHandler(std::string fileName)
+    : fileInfo{fileName, std::fstream(fileName)} {
+  std::cout << fileInfo.fileName
+            << (fileInfo.openFile.is_open() ? " opened\n" : " not opened\n");
 
-  // Add check here for fileName perhaps?
+  readFile(fileInfo.openFile);
 
-  this->openFile(this->fileInfo.fileName) ? std::cout << this->fileInfo.fileName << " opened\n"
-                                          : std::cout << this->fileInfo.fileName << " not opened\n";
+  cursorPosition = 0;
+
+  runCLI();
 }
 
 FileHandler::~FileHandler() {}
 
+/*
+May be used in the future
 int FileHandler::openFile(std::string fileName) {
-  this->fileInfo.openFile = std::ifstream(fileName);
+  fileInfo.openFile = std::ifstream(fileName);
 
-  if (this->fileInfo.openFile.is_open()) {
-    return 1;
-  } else {
-    return 0;
+  return fileInfo.openFile.is_open();
+}
+*/
+
+void FileHandler::readFile(std::fstream &fileHandle) {
+  outputBuffer << fileInfo.openFile.rdbuf();
+  std::cout << outputBuffer.str() << "\n";
+}
+
+void FileHandler::moveCursorForward() {
+  // Assuming each character is one byte
+  cursorPosition += sizeof(char);
+}
+
+void FileHandler::moveCursorBackward() {
+  // Assuming each character is one byte
+  if (cursorPosition >= sizeof(char)) {
+    cursorPosition -= sizeof(char);
   }
+}
+void FileHandler::displayCurrentLine() {
+  fileInfo.openFile.seekg(cursorPosition, std::ios::beg);
+
+  std::string currentLine;
+  std::getline(fileInfo.openFile, currentLine);
+  std::cout << "Cursor at position " << cursorPosition << ": " << currentLine
+            << "\n";
+}
+
+void FileHandler::runCLI() {
+  struct termios oldt, newt;
+  char ch;
+
+  // Save the current terminal settings
+  tcgetattr(STDIN_FILENO, &oldt);
+
+  // Disable canonical mode and echo
+  newt = oldt;
+  newt.c_lflag &= ~(ICANON | ECHO);
+  tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+
+  // Display the initial cursor position
+  displayCurrentLine();
+
+  // Listen for user input
+  while (1) {
+    std::cin.get(ch);
+
+    // Handle Vim-like motions
+    switch (ch) {
+      case 'h':
+        moveCursorBackward();
+        displayCurrentLine();
+        break;
+      case 'l':
+        moveCursorForward();
+        displayCurrentLine();
+        break;
+      case 'q':
+        // Quit the loop
+        goto endLoop;
+      default:
+        // Ignore other characters
+        break;
+    }
+  }
+
+endLoop:
+  // Restore the original terminal settings
+  tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
 }
--- fileHandler.hpp
+++ fileHandler.hpp
@@ -1,27 +1,36 @@
-#include <string>
-#include <fstream>
+#pragma once
 
+#include <fstream>
+#include <sstream>
+#include <string>
 
 struct FileInformation {
+  std::string fileName;
 
-    std::string fileName;
-    
-    std::ifstream openFile;
+  std::fstream openFile;
 };
 
 class FileHandler {
+ public:
+  FileHandler(std::string fileName);
+  ~FileHandler();
 
-    public:
-    FileHandler(std::string fileName);
-    ~FileHandler();
+  // Copy constructor
+  // Move constructor
 
-    // Copy constructor
-    // Move constructor 
+ private:
+  FileInformation fileInfo;
+  void readFile(std::fstream &fileHandle);
+  std::stringstream outputBuffer;
 
+  std::streampos cursorPosition;
 
-    private:
-    FileInformation fileInfo;
+  void moveCursorForward();
 
-    int openFile(std::string fileName);
+  void moveCursorBackward();
 
+  void displayCurrentLine();
+
+  void runCLI();
+  // int openFile(std::string fileName);
 };
